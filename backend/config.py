e1efdad7450b30d1d3bf70f20564b998a06731c4
Mrba-Software <cheikhtidianesamba99@gmail.com>
2025-07-17 21:22:41 +0200

    ✨ Mise à jour de la configuration de la base de données et ajout du script de migration

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///instance/app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #or 'sqlite:///instance/app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or 'uploads'
     MAX_CONTENT_LENGTH = int(os.environ.get('MAX_CONTENT_LENGTH') or 16 * 1024 * 1024)  # 16MB
@@ -22,7 +22,7 @@ class Config:
     ENV = os.environ.get('FLASK_ENV', 'production')
     if os.environ.get('CORS_ORIGINS'):
         CORS_ORIGINS = os.environ['CORS_ORIGINS'].split(',')
-    elif ENV == 'development':
-        CORS_ORIGINS = ['http://localhost:5173']
+    #elif ENV == 'development':
+        #CORS_ORIGINS = ['http://localhost:5173']
     else:
         CORS_ORIGINS = ['https://opencommunitymanager2.netlify.app']
---
+++ backend/migrate.py
@@ -0,0 +1,10 @@
+"""from app import create_app, db
+from flask_migrate import upgrade
+
+# Crée l'app avec le contexte approprié
+app = create_app()
+
+# Active le contexte de l'application pour accéder à la base
+with app.app_context():
+    upgrade()
+    print("✅ Migration appliquée avec succès.")"""
